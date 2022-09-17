88a7caeeb80822af8f95f14f2298531ce1c1cb31
warheartvik <vmarval88@gmail.com>
2022-09-17 16:06:40 -0500

    prueba frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Modal, Box, Button, Typography } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { FrmUsuario } from './components/FrmUsuario'
+import { UsuarioDetalle } from '../types/interfaces'
+
+// import './App.css';
+import './Funcional.css';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+// interface UsuarioDetalle {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+//   phone: string;
+//   website: string;
+// }
 function App() {
+  let [ListaUsuarios, setListaUsuarios] = useState<UsuarioDetalle[]>([]);
+
+  let [UsuarioId, setUsuarioId] = useState<number>(0)
+
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const abrirModal = () => setMostrarModal(true)
+  const cerraModal = () => setMostrarModal(false)
+
+  const Editar = (_UsuarioID: number) => {
+    setUsuarioId(_UsuarioID)
+    abrirModal()
+  }
+
+  const NuevoRegistro = () => {
+    setUsuarioId(0)
+    abrirModal()
+  }
+
+  let url = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setListaUsuarios(data);
+      })
+      .catch((error) => {
+        console.log("ERROR: ", error);
+      });
+  }, [url]);
+
+  const Agregar = async (Usuario: UsuarioDetalle ) => {
+    console.log('Agregar', Usuario)
+    await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      body: JSON.stringify(Usuario),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => {
+        if (response.status !== 201) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setListaUsuarios((ListaUsuarios) => [...ListaUsuarios, data]);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="contained" onClick={NuevoRegistro}>Nuevo usuario</Button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nombre</TableCell>
+              <TableCell align="right">Usuario</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Telefono</TableCell>
+              <TableCell align="right">Sitio web</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {ListaUsuarios.map((unUsuario) => (
+              <TableRow
+                key={unUsuario.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {unUsuario.name}
+                </TableCell>
+                <TableCell align="right">{unUsuario.username}</TableCell>
+                <TableCell align="right">{unUsuario.email}</TableCell>
+                <TableCell align="right">{unUsuario.phone}</TableCell>
+                <TableCell align="right">{unUsuario.website}</TableCell>
+                <TableCell align="right">
+                  <EditIcon onClick={()=>Editar(unUsuario.id)} />
+                  <DeleteIcon className="ml-1" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Modal
+        open={mostrarModal}
+        onClose={cerraModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <FrmUsuario 
+          guardarClic = {Agregar}
+          cancelarClic = {cerraModal}
+          id={UsuarioId} />
+        </Box>
+      </Modal>
     </div>
   );
 }
---
+++ src/components/FrmUsuario.tsx
@@ -0,0 +1,119 @@
+import { Button, Typography, TextField } from '@mui/material'
+import { useEffect, useState } from "react";
+import { UsuarioDetalle } from '../../types/interfaces'
+interface UsuarioId {
+    id: number,
+    cancelarClic: () => any
+    guardarClic: (usuario: UsuarioDetalle) => any
+}
+
+export function FrmUsuario(props: UsuarioId) {
+    let [UsuarioSelc, setUsuarioSelc] = useState<UsuarioDetalle>({
+        id: 0,
+        name: "",
+        username: "",
+        email: "",
+        phone: "",
+        website: ""
+    });
+    const Cancelar = (event: any) => {
+        props.cancelarClic();
+        event.preventDefault();
+    }
+    const inputCambio = (e: any) => {
+        const { name, value } = e.target;
+        setUsuarioSelc(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    const Guardar = (evt: any) => {
+        console.log('Guardar')
+        evt.preventDefault();
+        props.guardarClic(UsuarioSelc);
+
+    };
+    let url = `https://jsonplaceholder.typicode.com/users/${props.id}`;
+    useEffect(() => {
+        if (props.id > 0) {
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    setUsuarioSelc(data);
+                })
+                .catch((error) => {
+                    console.log("ERROR: ", error);
+                });
+        }
+    }, [url]);
+
+    return <div>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+            Usuario
+        </Typography>
+        <form onSubmit={Guardar}>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+
+
+
+                <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Nombre"
+                    variant="outlined"
+                    name="name"
+                    value={UsuarioSelc.name}
+                    onChange={inputCambio}
+                />
+                <TextField
+                    fullWidth
+                    className='mt-2'
+                    id="outlined-basic"
+                    label="Usuario"
+                    variant="outlined"
+                    name="username"
+                    value={UsuarioSelc.username}
+                    onChange={inputCambio}
+                />
+                <TextField
+                    fullWidth
+                    className='mt-2'
+                    id="outlined-basic"
+                    label="Email"
+                    variant="outlined"
+                    name="email"
+                    value={UsuarioSelc.email}
+                    onChange={inputCambio}
+                />
+                <TextField
+                    fullWidth
+                    className='mt-2'
+                    id="outlined-basic"
+                    label="Teléfono"
+                    variant="outlined"
+                    name="phone"
+                    value={UsuarioSelc.phone}
+                    onChange={inputCambio}
+                />
+                <TextField
+                    fullWidth
+                    className='mt-2'
+                    id="outlined-basic"
+                    label="Sitio web"
+                    variant="outlined"
+                    name="website"
+                    value={UsuarioSelc.website}
+                    onChange={inputCambio}
+                />
+            </Typography>
+            <div>
+                {/* <button onSubmit={handleOnSubmit}>Add</button> */}
+                <Button
+                    type="submit"
+                    variant="contained">Guardar</Button>
+                <Button variant="outlined" color="secondary" onClick={Cancelar}>Cancelar</Button>
+            </div>
+        </form>
+    </div>
+}
